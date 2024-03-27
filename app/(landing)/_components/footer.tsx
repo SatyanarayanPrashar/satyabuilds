d62bface51d95014c3f7148efafd89dd3aa6c64c
Satyanarayan Prashar <satyanarayanprashar73@gmail.com>
2024-03-28 02:07:43 +0530

    added links

--- app/(landing)/_components/footer.tsx
+++ app/(landing)/_components/footer.tsx
@@ -6,9 +6,15 @@ export const Footer = () => {
     return (
         <div className="w-[95%] flex justify-between items-center mt-[60px]">
             <div className="flex gap-3">
-                <Github size="20px"/>
-                <Linkedin size="20px"/>
-                <Twitter size="20px"/>
+                <a href="https://github.com/SatyanarayanPrashar" target="_blank" rel="noopener noreferrer" className="hover:text-[blue]">
+                    <Github size="20px"/>
+                </a>
+                <a href="https://www.linkedin.com/in/satyanarayan-prashar-57a170229/" target="_blank" rel="noopener noreferrer" className="hover:text-[blue]">
+                    <Linkedin size="20px"/>
+                </a>
+                <a href="https://twitter.com/Satya021Don" target="_blank" rel="noopener noreferrer" className="hover:text-[blue]">
+                    <Twitter size="20px"/>
+                </a>
             </div>
             <p>satyanarayan.pr29@gmail.com</p>
         </div>
