f738c044bb417e87512d3afb58f32411dff15747
Satyanarayan Prashar <satyanarayanprashar73@gmail.com>
2024-03-28 02:52:25 +0530

    added animation

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -6,6 +6,7 @@ import { Footer } from "./_components/footer";
 import { Heading } from "./_components/heading";
 import { useMediaQuery } from "usehooks-ts";
 import { LoadingBox } from "@/components/loading";
+import { Slide } from "react-awesome-reveal";
 
 const MarketingPage = () => {
   const isMobile = useMediaQuery("(max-width: 768px)");
@@ -31,8 +32,10 @@ const MarketingPage = () => {
   return (
     <div className="min-h-full flex flex-col">
       <div className={isMobile ? "block" : "flex flex-col items-center justify-center text-center gap-y-8 flex-1 pb-10 px-[45vh]"}>
-        <Heading />
-        <Feature />
+        <Slide delay={0.5} direction="up" triggerOnce={true}>
+          <Heading />
+          <Feature />
+        </Slide>
         <Footer />
       </div>
     </div>
